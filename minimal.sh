4c74148c68edfde022cbd9b381e613804193b2a0
lsdage <jbradavka@gmail.com>
2018-06-25 17:27:53 +0200

    upgrade Busybox version

--- minimal.sh
+++ minimal.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -ex
 KERNEL_VERSION=4.16.9
-BUSYBOX_VERSION=1.28.3
+BUSYBOX_VERSION=1.28.4
 SYSLINUX_VERSION=6.03
 wget -O kernel.tar.xz http://kernel.org/pub/linux/kernel/v4.x/linux-$KERNEL_VERSION.tar.xz
 wget -O busybox.tar.bz2 http://busybox.net/downloads/busybox-$BUSYBOX_VERSION.tar.bz2
